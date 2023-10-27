78c2aec436d37196ad90fdf0a1b8d7d43c9ec07b
MarSerMer <marinakms@yandex.ru>
2023-10-27 15:28:26 +0300

    Homework 4

---
+++ src/main/java/ticket/PaymentProvider.java
@@ -0,0 +1,10 @@
+package ticket;
+
+public class PaymentProvider {
+
+    public boolean buyTicket(int orderId, String cardNo, double amount){
+        // Посылаем запрос на формирование заявки на проведение платежа в процессинговую компанию (ProcessingCompany)
+        System.out.println("Билетик куплен.");
+        return true;
+    }
+}
